5351c737b3cf004a1f986de41e635f555a26f943
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-12 17:32:15 -0300

    Update tipagemFraca.js

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'legal'
+console.log(qualquer);
+console.log(typeof qualquer); //typeof indica o tipo da variavel
+
+
+qualquer = 3.4
+console.log(qualquer);
+console.log(typeof qualquer);
+
