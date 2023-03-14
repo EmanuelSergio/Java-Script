d8e088f81e9617d14032e27385ebf93953daeeb8
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-13 23:25:32 -0300

    Create templateString.js

---
+++ Fundamentos/templateString.js
@@ -0,0 +1,14 @@
+const nome = 'emanuel'
+const concatenacao = 'ola ' +nome +'!'
+const template = `
+    ola que tal
+    amigo
+    querido
+    ${nome}!
+`
+
+console.log(concatenacao, template);
+
+//expressoes
+
+console.log(` 1 + 1 = ${1+1}`);
