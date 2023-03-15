d71ba096e8353d4083dcbf3a66578679f78ab701
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-15 00:04:03 -0300

    Update nullUndefined.js

--- Fundamentos/nullUndefined.js
+++ Fundamentos/nullUndefined.js
@@ -4,4 +4,26 @@ console.log(valor);
 valor = null
 
 console.log(valor); //ausência de valor 
-    
+//console.log(valor.toString());
+
+const produto = {}
+console.log(produto.preco); //undefined o preco não esta definido
+
+console.log(produto);
+
+produto.preco = 3.5
+
+console.log(produto);
+
+produto.preco = undefined //evitar atribuir undefined
+
+console.log(!!produto.preco);
+console.log(produto);
+
+produto.preco = null
+console.log(!!produto.preco);
+
+console.log(produto);
+
+
+
