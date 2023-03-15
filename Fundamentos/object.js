d5e1b44e7276313c6e03ca1e1295629c73a1ae5c
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-14 23:43:45 -0300

    Criando objetos

---
+++ Fundamentos/object.js
@@ -0,0 +1,18 @@
+const prod1 = {}
+prod1.nome = 'celular ultra mega'
+prod1.preco = 1000
+prod1['desconto legal'] = 0.40 //evitar atributos com espaço
+
+
+
+console.log(prod1);
+
+const prod2 = {
+
+nome: 'Camisa polo',
+preco: 79.80
+
+}
+
+
+console.log(prod2);
