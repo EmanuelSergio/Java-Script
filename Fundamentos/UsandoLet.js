5db4a015afe3a5038e928a8e91a0b2759724323a
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-15 23:04:58 -0300

    Usando Let

---
+++ Fundamentos/UsandoLet.js
@@ -0,0 +1,10 @@
+var numero = 1
+
+{
+let numero = 2 
+console.log('dentro =',numero);//procura no escopo interno primeiro (dentro do bloco do codigo)
+
+}
+//escopos diferentes
+console.log('fora =', numero);
+
