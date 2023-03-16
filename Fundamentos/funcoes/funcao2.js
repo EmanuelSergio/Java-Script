1a3ed6d16089f43690a19078efcdc9ddac95e79b
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-15 22:38:40 -0300

    retorno implícito

--- Fundamentos/funcoes/funcao2.js
+++ Fundamentos/funcoes/funcao2.js
@@ -7,3 +7,19 @@ const imprimirSoma = function(a,b){
 imprimirSoma(2,3)
 
 //armazenando uma função arrow em uma variavel 
+
+const soma = (a,b) => {
+    return a+b
+}
+
+console.log(soma(2,5));
+
+//retorno implícito
+
+const subtracao = (a,b) => a - b //parece lambda
+
+console.log(subtracao(10,5));
+
+
+
+
