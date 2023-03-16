db3edc4dbf5c37e3ae14d1a8c2323d72c0c75364
EmanuelSergio <104005334+EmanuelSergio@users.noreply.github.com>
2023-03-15 23:01:28 -0300

    Usando Var e escopo universal

---
+++ Fundamentos/Var/usandoVar.js
@@ -0,0 +1,26 @@
+{
+    {
+        {
+            {
+                {
+                    var sera ='será???'
+                    console.log(sera);
+                }
+            }
+        }
+    }
+}
+
+
+
+console.log(sera);
+
+function teste() {
+    
+var local = 123
+teste()
+}
+
+
+
+//console.log(teste());//não está visivel fora da função (o var)
---
+++ Fundamentos/Var/usandoVar2.js
@@ -0,0 +1,16 @@
+var numero = 1
+
+{
+var numero = 2//sobscreveu a variavel (não possui escopo universal em bloco de codigo)
+console.log('dentro =', numero);
+
+}
+
+console.log('fora =', numero);
+
+
+
+
+
+
+
