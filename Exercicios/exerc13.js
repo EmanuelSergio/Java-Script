e80cb516fc5b9cee477a56ce4bd8da21a730ec42
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2023-09-25 14:43:31 -0300

    Update exerc13.js

--- Exercicios/exerc13.js
+++ Exercicios/exerc13.js
@@ -6,10 +6,23 @@ function util(dia) {
     switch (dia) {
         case 1: 
             return console.log('domingo');
+        case 2:
+            return console.log('segunda');       
+        case 3: 
+            return console.log('terça');
+        case 4:
+            return console.log('quarta');
+        case 5: 
+            return console.log('quinta');
+        case 6:
+            return console.log('sexta');
+        case 7: 
+            return console.log('sabado');
         default:
+            console.log('insira um dia');
             break;
     }
 
 }
 
-util(1)
+util(3)
